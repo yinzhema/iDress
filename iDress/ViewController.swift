383ecaa8e9ef4f55a33d708e99fdfcd1d87acdb3
yinzhema <yma9826@gmail.com>
2019-12-02 17:31:10 -0500

    Initial Commit

---
+++ iDress/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iDress
+//
+//  Created by Yinzhe Ma on 12/2/19.
+//  Copyright © 2019 Yinzhe Ma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
